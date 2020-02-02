cb040a114925890b16b3691e1d01624c2f52eea6
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-02 12:13:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ monopoly = {
 	      :three_pieces_owned => "100",
 	      :four_pieces_owned => "200"
 	    }
-	  
+	  }
 	}	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
@@ -48,7 +48,7 @@ def monopoly_with_third_tier
 	                         :three_pieces_owned => "100",
 	                         :four_pieces_owned => "200"
 	    }
-	  
+	  }
 	}	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
@@ -65,7 +65,7 @@ def monopoly_with_fourth_tier
 	                         :two_pieces_owned => "50",
 	                         :three_pieces_owned => "100",
 	                         :four_pieces_owned => "200"
-	    
+	   } 
 	  }
 	}#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
