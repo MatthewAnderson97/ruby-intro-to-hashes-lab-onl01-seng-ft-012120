b715ef4ae6254130ac025724c457ca5aaa639b45
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-02 12:19:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,8 +39,8 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => 4,
-	    :names => { :reading_railroads => {}
+	    :pieces=>4,
+	    :names=>{ :reading_railroads => {}
 	                :pennsylvania_railroad => {}
 	                :b_and_o_railroad => {}
 	                :shortline => {}}
