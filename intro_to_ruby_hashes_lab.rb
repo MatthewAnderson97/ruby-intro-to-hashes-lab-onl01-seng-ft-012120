da206a2ee4a8559591e0265fb5c1e36e4fb6fc73
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-02 12:26:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_second_tier
 monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :names => {}
+	    :names => {},
 	      :rent_in_dollars => { :one_piece_owned => 25,
 	      :two_pieces_owned => 50,
 	      :three_pieces_owned => 100,
@@ -43,7 +43,7 @@ def monopoly_with_third_tier
 	    :names =>{ :reading_railroads => {}
 	                :pennsylvania_railroad => {}
 	                :b_and_o_railroad => {}
-	                :shortline => {}}
+	                :shortline => {}},
 	   :rent_in_dollars => { :one_piece_owned => 20,
 	                         :two_pieces_owned => 50,
 	                         :three_pieces_owned => 100,
@@ -62,7 +62,7 @@ def monopoly_with_fourth_tier
 	    :names => { :reading_railroads => {"mortgage_value" =>"$100"},
 	                :pennsylvania_railroad => {"mortgage_value" =>"$200"},
 	                :b_and_o_railroad => {"mortgage_value" => "$400"},
-	                :shortline => {"mortgage_value" => "$800"}}
+	                :shortline => {"mortgage_value" => "$800"}},
 	   :rent_in_dollars => { :one_piece_owned => 20,
 	                         :two_pieces_owned => 50,
 	                         :three_pieces_owned => 100,
