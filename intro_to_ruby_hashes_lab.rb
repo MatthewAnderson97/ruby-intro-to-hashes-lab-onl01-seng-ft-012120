704cb3209f8aabb967eefe784ff800885b526b98
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-02 12:32:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ def monopoly_with_third_tier
 	                :pennsylvania_railroad => {},
 	                :b_and_o_railroad => {},
 	                :shortline => {}},
-	   :rent_in_dollars => { :one_piece_owned => 20,
+	   :rent_in_dollars => { :one_piece_owned => 25,
 	                         :two_pieces_owned => 50,
 	                         :three_pieces_owned => 100,
 	                         :four_pieces_owned => 200
@@ -63,7 +63,7 @@ def monopoly_with_fourth_tier
 	                :pennsylvania_railroad => {"mortgage_value" =>"$200"},
 	                :b_and_o_railroad => {"mortgage_value" => "$400"},
 	                :shortline => {"mortgage_value" => "$800"}},
-	   :rent_in_dollars => { :one_piece_owned => 20,
+	   :rent_in_dollars => { :one_piece_owned => 25,
 	                         :two_pieces_owned => 50,
 	                         :three_pieces_owned => 100,
 	                         :four_pieces_owned => 200
